2d4018606dbe174c0804559c1de46f0a17030751
sherrycayheyhey <sherrycaywood@yahoo.com>
2020-05-18 10:52:39 -0700

    All categories displaying respective word lists. To do this the ColorsActivity, PhrasesActivity, and FamilyActivity were updated to use the content view word_list (formerly the one created for NumbersActivity but renamed so it could be reused for everyone), ArrayLists of Word objects were added, and then a WordAdapter was created and combined with a ListView. Also deleted the old layout files since word_list.xml is being used instead.

--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -17,12 +17,33 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class FamilyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_family);
+        setContentView(R.layout.word_list);
+
+        //ArrayList for list of colors in default (English) and Miwok
+        ArrayList<Word> words = new ArrayList<>();
+        words.add(new Word("father", "әpә"));
+        words.add(new Word("mother", "әṭa"));
+        words.add(new Word("son","angsi"));
+        words.add(new Word("daughter", "tune"));
+        words.add(new Word("older brother", "taachi"));
+        words.add(new Word("younger brother", "chalitti"));
+        words.add(new Word("older sister", "teṭe"));
+        words.add(new Word("younger sister", "kolliti"));
+        words.add(new Word("grandmother", "ama"));
+        words.add(new Word("grandfather", "paapa"));
+
+        //create a WordAdapter whose data source is an ArrayList of Word objects then combine it with the ListView
+        WordAdapter adapter = new WordAdapter(this, words);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,11 +17,7 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -30,7 +26,7 @@ public class NumbersActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_numbers);
+        setContentView(R.layout.word_list);
 
         /* array vs ArrayList
          *
@@ -95,7 +91,7 @@ public class NumbersActivity extends AppCompatActivity {
         // set to display the two words (and image)
         //the 2 things passed in here are the context (this = NumbersActivity) and the ArrayList of words
         WordAdapter adapter = new WordAdapter(this, words);
-        //find the ListView object (ListView with id "list") from the activity_numbers.xml
+        //find the ListView object (ListView with id "list") from the word_list.xml
         ListView listView = (ListView) findViewById(R.id.list);
         //make the listView use the ArrayAdapter created above so the ListView will display the list items
         //for each Word object in the ArrayList (words), aka, associate the ListView with the ArrayAdapter
